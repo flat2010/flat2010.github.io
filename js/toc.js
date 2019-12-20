75058347b12187595019ad43ffbb5dddc046847b
flat2010 <idenbe@gmail.com>
2019-12-21 01:01:20 +0800

    Site updated: 2019-12-21 01:01:17

--- js/toc.js
+++
@@ -1,7 +0,0 @@
-function addIcons(){
-    var $itemHasChild = $("#toc .toc-item:has(> .toc-child)");
-    var $titleHasChild = $itemHasChild.children(".toc-link");
-    $itemHasChild.prepend("<i class='fa fa-caret-down'></i><i class='fa fa-caret-right'></i>");
-    var $iconToFold = $(".toc-item > .fa-caret-down");
-    $iconToExpand.addClass("hide");
-}
